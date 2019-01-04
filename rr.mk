394216aff5307137b9d0a1fbbadbca53224dc38b
Razhor <zamorano0695@gmail.com>
2019-01-03 19:12:17 -0700

    Resurrection Remix bringup

--- rr.mk
+++ rr.mk
@@ -16,7 +16,7 @@
 $(call inherit-product, device/motorola/harpia/full_harpia.mk)
 
 # Inherit some common LOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
 # Boot animation
 TARGET_SCREEN_WIDTH := 720
@@ -26,7 +26,7 @@ TARGET_BOOTANIMATION_HALF_RES := true
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := harpia
-PRODUCT_NAME := lineage_harpia
+PRODUCT_NAME := rr_harpia
 PRODUCT_MODEL := Moto G Play
 PRODUCT_BRAND := Motorola
 PRODUCT_MANUFACTURER := Motorola
